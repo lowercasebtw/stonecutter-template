3dfea817a99df7040e9222c1bc99b2177f23e58b
lowercasebtw <ignzoudy@gmail.com>
2025-11-23 15:04:46 -0500

    Initial Commit

---
+++ src/main/java/btw/lowercase/template/TemplateFabricClient.java
@@ -0,0 +1,11 @@
+package btw.lowercase.template;
+
+import dev.kikugie.fletching_table.annotation.fabric.Entrypoint;
+import net.fabricmc.api.ClientModInitializer;
+
+@Entrypoint
+public final class TemplateFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ stonecutter.gradle.kts
@@ -0,0 +1,36 @@
+import dev.kikugie.stonecutter.data.tree.struct.ProjectNode
+
+plugins {
+    id("dev.kikugie.stonecutter")
+    alias(libs.plugins.publishing)
+    alias(libs.plugins.spotless)
+}
+
+stonecutter active "1.21.10-fabric" /* [SC] DO NOT EDIT */
+
+stonecutter tasks {
+    val ordering = Comparator
+        .comparing<ProjectNode, _> { stonecutter.parse(it.metadata.version) }
+        .thenComparingInt { if (it.metadata.project.endsWith("fabric")) 1 else 0 }
+    order("publishMods", ordering)
+}
+
+tasks.named("publishMods") {
+    group = "build"
+}
+
+// Header
+spotless {
+    val licenseHeader = rootProject.file("HEADER")
+    lineEndings = com.diffplug.spotless.LineEnding.UNIX
+
+    java {
+        licenseHeaderFile(licenseHeader)
+        target("src/**/*.java", "versions/*/src/**/*.java")
+    }
+
+    kotlin {
+        licenseHeaderFile(licenseHeader)
+        target("src/**/*.kt", "versions/*/src/**/*.kt")
+    }
+}
